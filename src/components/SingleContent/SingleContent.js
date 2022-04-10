9ba4074379dd332dfda73f99c5464d7461d61b67
Tanvir <tanvirahmed1418@gmail.com>
2022-04-10 16:35:28 +0600

    Modal and Carousel for Movie Component

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,6 +1,7 @@
 import { Badge } from '@material-ui/core';
 import React from 'react';
 import { img_300, unavailable } from '../../config/config';
+import ContentModal from '../ContentModal/ContentModal';
 
 import './SingleContent.css';
 
@@ -13,7 +14,7 @@ const SingleContent = ({
   vote_average,
 }) => {
   return (
-    <div className="media">
+    <ContentModal media_type={media_type} id={id}>
       <Badge
         badgeContent={vote_average}
         color={vote_average > 6 ? 'primary' : 'secondary'}
@@ -28,7 +29,7 @@ const SingleContent = ({
         {media_type === 'tv' ? 'TV Series' : 'Movie'}
         <span className="subTitle">{date}</span>
       </span>
-    </div>
+    </ContentModal>
   );
 };
 
